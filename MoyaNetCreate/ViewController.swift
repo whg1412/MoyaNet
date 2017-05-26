a9f70e963357d049895b6859e3f6dd67de11df17
hg w <wanghuiguang@tton.co>
2017-05-26 16:26:18 +0800

    新建工程

---
+++ MoyaNetCreate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoyaNetCreate
+//
+//  Created by hg w on 2017/5/26.
+//  Copyright © 2017年 whg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
